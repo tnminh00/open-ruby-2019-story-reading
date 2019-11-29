b148dfd868a85f6a28952614373da1036a04dbc3
Tran Nhat Minh <tn.minh00@gmail.com>
2019-11-29 09:51:36 +0700

    Delete story

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,10 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
     Rails.application.routes.default_url_options[:locale] = I18n.locale
   end
+
+  def check_is_admin
+    return if current_user.is_admin?
+    flash[:danger] = t ".not_admin"
+    redirect_to root_path
+  end
 end
