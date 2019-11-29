03dc7b2dfcf642b4be4084b5c7755b07901ccff1
Tran Nhat Minh <tn.minh00@gmail.com>
2019-11-29 09:51:36 +0700

    Search stories

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,9 @@
+class SearchController < ApplicationController
+  def index
+    if params[:form_search].present?
+      @search_stories = Story.search_by_name(params[:form_search]).page(params[:page]).per Settings.perpage
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+end
